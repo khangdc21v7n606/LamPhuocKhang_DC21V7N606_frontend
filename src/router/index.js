a62187bb42c4ac3cb2b2f526065d41c6432400a8
khangdc21v7n606 <khangdc21v7n606@vlvh.ctu.edu.vn>
2025-10-08 23:28:10 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,12 @@ import ContactBook from "@/views/ContactBook.vue";
         component: ContactBook,
  },
         {
+         path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+},
+        {
         path: "/:pathMatch(.*)*",
         name: "notfound",
         component: () => import("@/views/NotFound.vue"),       
